09bb436c5456c6534547d77432dd577aa1a08447
dmauricio4 <diegomarshall4@gmail.com>
2023-04-20 15:07:29 -0500

    Create repository

---
+++ src/test/java/ec/edu/ups/crudapi/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.ups.crudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
